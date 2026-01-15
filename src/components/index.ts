9c53062817308048aee30437a9531da804694f63
Pedrox <95978744+BasedCaveman@users.noreply.github.com>
2026-01-15 15:28:39 -0300

    Clean up exports in index.ts
    Removed multiple exports from index.ts, keeping only LocationSelector.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,3 @@
 export { Header } from "./Header";
 export { MarketCard } from "./MarketCard";
-export { PredictionModal } from "./PredictionModal";
 export { LocationSelector } from "./LocationSelector";
-export { CurrencySelector } from "./CurrencySelector";
-export { AccountModal, AccountButton } from "./Account";
-export { DepositWithdrawModal } from "./DepositWithdraw";
-export { BettingWindowStatus, CompactCountdown } from "./BettingWindowStatus";
-export { OracleResolutionStatus, CompactResolutionStatus } from "./OracleResolutionStatus";
